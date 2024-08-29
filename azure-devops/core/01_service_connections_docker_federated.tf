69cdb759042af0ac8ba22ecd3a0453c26702ef31
diegoitaliait <diego.lagosmorales@pagopa.it>
2024-08-29 12:10:54 +0200

    workload identities are data from outside

--- azure-devops/core/01_service_connections_docker_federated.tf
+++ azure-devops/core/01_service_connections_docker_federated.tf
@@ -1,63 +1,17 @@
-# 🟢 DEV
-module "dev_azurecr_service_conn" {
-  source = "./.terraform/modules/__devops_v0__/azuredevops_serviceendpoint_azurecr_federated"
-  providers = {
-    azurerm = azurerm.dev
-  }
-
-  project_id = local.devops_project_id
-  # #tfsec:ignore:general-secrets-no-plaintext-exposure
-  serviceendpoint_azurecr_name_prefix = "${local.dev_docker_registry_name}-docker"
-
-  tenant_id         = data.azurerm_client_config.current.tenant_id
-  subscription_id   = data.azurerm_subscriptions.dev.subscriptions[0].subscription_id
-  subscription_name = data.azurerm_subscriptions.dev.subscriptions[0].display_name
-
-  location            = local.location_service_conn
-  resource_group_name = local.dev_identity_rg_name
-
-  azurecr_name                = local.dev_docker_registry_name
-  azurecr_resource_group_name = local.dev_docker_registry_rg_name
+#
+# ACR Workload Identity
+#
+data "azuredevops_serviceendpoint_azurecr" "dev_ita_workload_identity" {
+  project_id            = data.azuredevops_project.this.id
+  service_endpoint_name = var.acr_ita_service_connection_workload_identity_dev
 }
 
-# 🟨 UAT
-module "uat_azurecr_service_conn" {
-  source = "./.terraform/modules/__devops_v0__/azuredevops_serviceendpoint_azurecr_federated"
-  providers = {
-    azurerm = azurerm.uat
-  }
-
-  project_id = local.devops_project_id
-  # #tfsec:ignore:general-secrets-no-plaintext-exposure
-  serviceendpoint_azurecr_name_prefix = "${local.uat_docker_registry_name}-docker"
-
-  tenant_id         = data.azurerm_client_config.current.tenant_id
-  subscription_id   = data.azurerm_subscriptions.uat.subscriptions[0].subscription_id
-  subscription_name = data.azurerm_subscriptions.uat.subscriptions[0].display_name
-
-  location                    = local.location_service_conn
-  resource_group_name         = local.uat_identity_rg_name
-  azurecr_name                = local.uat_docker_registry_name
-  azurecr_resource_group_name = local.uat_docker_registry_rg_name
+data "azuredevops_serviceendpoint_azurecr" "uat_ita_workload_identity" {
+  project_id            = data.azuredevops_project.this.id
+  service_endpoint_name = var.acr_ita_service_connection_workload_identity_uat
 }
 
-#module "prod_azurecr_service_conn" {
-#  source = "./.terraform/modules/__devops_v0__/azuredevops_serviceendpoint_federated"
-#  providers = {
-#    azurerm = azurerm.prod
-#  }
-#
-#  project_id = local.devops_project_id
-#  # #tfsec:ignore:general-secrets-no-plaintext-exposure
-#  name = "${data.azurerm_subscriptions.prod.subscriptions[0].display_name}-AZURE"
-#
-#  tenant_id         = data.azurerm_client_config.current.tenant_id
-#  subscription_id   = data.azurerm_subscriptions.prod.subscriptions[0].subscription_id
-#  subscription_name = data.azurerm_subscriptions.prod.subscriptions[0].display_name
-#
-#  location               = local.location_service_conn
-#  resource_group_name    = local.prod_identity_rg_name
-#  check_approval_enabled = true
-#  approver_ids           = data.azuredevops_group.admins.origin_id
-#}
-
+data "azuredevops_serviceendpoint_azurecr" "prod_ita_workload_identity" {
+  project_id            = data.azuredevops_project.this.id
+  service_endpoint_name = var.acr_ita_service_connection_workload_identity_prod
+}
