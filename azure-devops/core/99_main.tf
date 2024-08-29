66a4ba7feb300c6b58e44e71c87549273b7cf732
diegoitaliait <diego.lagosmorales@pagopa.it>
2024-08-29 12:11:39 +0200

    provider updated to 1.2.0 azdo

--- azure-devops/core/99_main.tf
+++ azure-devops/core/99_main.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     azuredevops = {
       source  = "microsoft/azuredevops"
-      version = "~> 1.1"
+      version = "~> 1.2.0"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
